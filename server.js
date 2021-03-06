fddc60401f477ee6a7b69973c214ef8740758e9f
unknown <manyuabi555@gmail.com>
2021-03-06 12:19:56 +0700

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
+
+router.get("/", (req, res) => {
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+ });
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
